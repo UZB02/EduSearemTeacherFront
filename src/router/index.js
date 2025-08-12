8324e5d721b7cbaf84f1bd1ac376c43d730ca1c1
UZB02 <muhsinbekmirzamatov3@gmail.com>
2025-08-12 13:18:20 +0500

    tekshiruv

--- src/router/index.js
+++ src/router/index.js
@@ -28,10 +28,7 @@ const router = createRouter({
     {
       path: '/login',
       name: 'Login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/auth/LoginView.vue'),
+      component: () => import('../views/auth/loginView.vue'),
       meta: {
         layout: AuthLayout,
       },
